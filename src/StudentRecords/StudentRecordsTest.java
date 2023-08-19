e9d82f29c5aabf8643a3ff8685357dae96116aa4
Ivan Doma <102655208+IvanNikolovBT@users.noreply.github.com>
2023-08-19 17:04:50 +0200

    commit

--- src/StudentRecords/StudentRecordsTest.java
+++ src/StudentRecords/StudentRecordsTest.java
@@ -1,149 +1,11 @@
-package StudentRecords;////package StudentRecords;
-//
-//import java.io.*;
-//import java.util.*;
-//import java.util.stream.IntStream;
-//
-//public class StudentRecordsTest {
-//    public static void main(String[] args) {
-//        System.out.println("=== READING RECORDS ===");
-//        StudentRecords studentRecords = new StudentRecords();
-//        int total = studentRecords.readRecords(System.in);
-//        System.out.printf("Total records: %d\n", total);
-//        System.out.println("=== WRITING TABLE ===");
-//        studentRecords.writeTable(System.out);
-//        System.out.println("=== WRITING DISTRIBUTION ===");
-//        studentRecords.writeDistribution(System.out);
-//    }
-//}
-//
-//class Student {
-//    List<Integer> grades;
-//    double avg;
-//
-//    String id;
-//    public Student(String id) {
-//        this.grades = new ArrayList<>();
-//        fill0();
-//        this.avg = 0;
-//        this.id=id;
-//    }
-//
-//    private void fill0() {
-//        for(int i=0;i<5;i++)
-//            grades.add(0);
-//    }
-//
-//    public void add(int integer) {
-//        grades.set(10-integer,grades.get(10-integer)+1);
-////        System.out.println(grades.get(10-integer).toString());
-//    }
-//
-//    public void calculateAvg() {
-//
-//        int sum = IntStream.range(0,5).map(i->grades.get(i)*convert(i)).sum();
-//        int c= IntStream.range(0,5).map(i->grades.get(i)).sum();
-//                avg = 1. * sum / c;
-//    }
-//    int convert(int i)
-//    {
-//       switch (i)
-//       {
-//           case 0:
-//               return 10;
-//           case 1:
-//               return 9;
-//           case 2:
-//               return 8;
-//           case 3:
-//               return 7;
-//           default:
-//               return 6;
-//       }
-//    }
-//
-//    public double getAvg() {
-//        return avg;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return String.format("%s %.2f",id,getAvg());
-//    }
-//
-//    public  String getId()
-//    {
-//        return  id;
-//    }
-//}
-//
-//class StudentRecords {
-//
-//    Map<String,List<Student>> studentsByCategory;
-//    public StudentRecords()
-//    {
-//        studentsByCategory=new TreeMap<>();
-//    }
-//    public int readRecords(InputStream inputStream) {
-//        Scanner scanner=new Scanner(inputStream);
-//        while (scanner.hasNextLine())
-//        {
-//            String line=scanner.nextLine();
-//            String[]splitter=line.split("\\s+");
-//            String id=splitter[0];
-//            String category=splitter[1];
-//            int n=splitter.length-2;
-//            Student student=new Student(id);
-//            for(int i=0;i<n;i++)
-//            {
-//                if(!splitter[i+2].equals("...snip..."))
-//                student.add(Integer.parseInt(splitter[i+2]));
-//            }
-//            student.calculateAvg();
-//            studentsByCategory.putIfAbsent(category,new ArrayList<>());
-//            studentsByCategory.computeIfPresent(category,(k,v)->{v.add(student);
-//            return v;});
-//
-//        }
-//        return getRecordsLen();
-//    }
-//    public int getRecordsLen()
-//    {
-//        return  studentsByCategory.values().stream().mapToInt(i->i.size()).sum();
-//    }
-//    public void writeTable(OutputStream outputStream) {
-//        //spored nasoka gi pechati grupirani
-//        //prven se pechati imeto na nasokata
-//        //site zapisi za studentite od taa nasoka
-//        //sortirani spored prosek vo opagacki redosled
-//        //prosekot e decimanel broj
-//        PrintWriter printWriter=new PrintWriter(outputStream);
-//        for(int i=0;i<studentsByCategory.size();i++)
-//        {
-//            Set<String> keys=studentsByCategory.keySet();
-//             String []kluc= (String[]) keys.toArray();
-//            printWriter.println(kluc[i]);
-//            for()
-//        }
-//        studentsByCategory.values().stream().forEach(i->i.stream().sorted(Comparator.comparing(Student::getAvg,Comparator.reverseOrder()).thenComparing(Student::getId)).forEach(a->printWriter.println(a.toString())));
-//        printWriter.flush();
-//
-//
-//    }
-//
-//    public void writeDistribution(OutputStream ot) {
-//
-//    }
-//}
+//package StudentRecords;
+
 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.*;
 
-/**
- * January 2016 Exam problem 1
- */
 public class StudentRecordsTest {
     public static void main(String[] args) {
         System.out.println("=== READING RECORDS ===");
@@ -157,125 +19,157 @@ public class StudentRecordsTest {
     }
 }
 
-class StudentRecords {
+class Student {
+    String code;
+    String path;
+    List<Integer> grades;
 
+    public Student(String code, String path, List<Integer> grades) {
+        this.code = code;
+        this.path = path;
+        this.grades = new ArrayList<>();
+    }
+
+    public Student(String s) {
+        String[] splitter = s.split("\\s+");
+        grades=new ArrayList<>();
+        code = splitter[0];
+        path = splitter[1];
+        for (int i = 2; i < splitter.length; i++) {
+            if(!splitter[i].equals("...snip..."))
+            grades.add(Integer.parseInt(splitter[i]));
+        }
+    }
+
+    public double getAvg() {
+        return grades.stream().mapToInt(i -> i).average().orElse(0);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f", code, getAvg());
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
+
+class Path {
+    String path;
+
+    HashMap<Integer, Integer> grades;
+
+    public Path(String path) {
+        this.path = path;
+        grades = new HashMap<>();
+
+    }
+
+    public void addGrade(int grade) {
+        if (grades.containsKey(grade))
+            grades.put(grade, grades.get(grade) + 1);
+        else
+            grades.put(grade, 1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(path).append("\n");
+        for (int i = 6; i <= 10; i++) {
+            stringBuilder.append(String.format("%2d | ", i));
+            stringBuilder.append(printStars(i));
+            stringBuilder.append("(").append(grades.get(i)).append(")");
+            if(i!=10)
+                stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private String printStars(int i) {
+        String a = "";
+        int number = grades.get(i);
+        int val = (int) Math.ceil((double) grades.get(i) / 10);
+        for (int j = 0; j < val; j++) {
+            a += "*";
+        }
+        return a;
+    }
+    public int get10()
+    {
+        return grades.get(10);
+    }
+
+}
+
+class StudentRecords {
     Map<String, List<Student>> students;
-    Map<String, Stat > distirbution;
+    Map<String, Path> paths;
 
     public StudentRecords() {
         students = new TreeMap<>();
-        distirbution = new TreeMap<>();
+        paths = new TreeMap<>();
+    }
+
+    int readRecords(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream);
+        int i = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Student student = new Student(line);
+            studentsWork(student);
+            pathWork(student);
+            i++;
+        }
+        return i;
+    }
+
+    private void studentsWork(Student student) {
+        if (!students.containsKey(student.path))
+            students.put(student.path, new ArrayList<>());
+
+        students.computeIfPresent(student.path, (k, v) -> {
+            v.add(student);
+            return v;
+        });
+    }
+
+    private void pathWork(Student student) {
+        Path path;
+        if (!paths.containsKey(student.path))
+            path = new Path(student.path);
+        else
+            path = paths.get(student.path);
+        for (int i = 0; i < student.grades.size(); i++)
+            path.addGrade(student.grades.get(i));
+        paths.put(student.path, path);
     }
 
     public void writeTable(OutputStream outputStream) {
         PrintWriter printWriter = new PrintWriter(outputStream);
-        for (String key : students.keySet()) {
-            printWriter.println(key);
-            //mozi ovde proveri
-            for (Student student : students.get(key))
-                printWriter.println(student);
+        //ovde spored nasoka treba
+        //znachi prvo treba imeto na nasokata da se ispechati
+        //spored prosekot vo opagjachki red
+        //treba mapa kaj sho kluchot e nasokata
+        for (Map.Entry<String, List<Student>> stringListEntry : students.entrySet()) {
+            printWriter.println(stringListEntry.getKey());
+            stringListEntry.getValue().stream().sorted(Comparator.comparing(Student::getAvg).reversed().thenComparing(Student::getCode)).forEach(i -> printWriter.println(i.toString()));
         }
         printWriter.flush();
     }
 
     public void writeDistribution(OutputStream outputStream) {
         PrintWriter printWriter = new PrintWriter(outputStream);
-
-        for (int i = 6; i < 11; i++)
-        {
-            printWriter.print(String.format("%2d |"));
-
-        }
+        //brojot na ocenki po nasoka
+        //sortirani spored brojot na desetki(rastechki)
+        //mozi da se klaj kako kluch pak nasokata
+        //ama da se stavi nova klasa vnatre
+        //za da ima dobra print metoda i site podatoci da se postapni
+        //lesno
+        paths.values().stream().sorted(Comparator.comparing(Path::get10).reversed()).forEach(printWriter::println);
         printWriter.flush();
-
     }
 
-    private class Stat {
-        TreeMap<Integer,Integer>num;
-
-
-        public Stat() {
-           num=new TreeMap<>();
-
-        }
-        public void add(int n)
-        {
-            num.computeIfPresent(n,(k,v)->v+1);
-            num.putIfAbsent(n,1);
-        }
-
-
-    }
-
-    public int readRecords(InputStream in) {
-        Scanner scanner = new Scanner(in);
-        int total = 0;
-        while (scanner.hasNextLine()) {
-            total++;
-            String line = scanner.nextLine();
-            String[] splitter = line.split("\\s+");
-            String id = splitter[0];
-            String category = splitter[1];
-            int n = splitter.length - 2;
-            Student student = new Student(id);
-            for (int i = 0; i < n; i++) {
-                int index = i + 2;
-                if (!splitter[index].equals("...snip...")) {
-                    int value = Integer.parseInt(splitter[index]);
-                    student.addGrade(value);
-                    distirbution.putIfAbsent(category, new Stat());
-                    distirbution.computeIfPresent(category,(k,v)-> {v.add(value);
-                    return v;});
-                }
-            }
-            students.putIfAbsent(category, new ArrayList<>());
-            students.computeIfPresent(category, (k, v) -> {
-                v.add(student);
-                return v;
-            });
-        }
-        return total;
-    }
 }
 
-class Student implements Comparable<Student> {
-    String id;
-    int sum;
-    List<Integer> grades;
-
-    public Student(String id) {
-        this.id = id;
-        sum = 0;
-        grades = new ArrayList<>();
-    }
-
-    public void addGrade(int a) {
-        sum += a;
-        grades.add(a);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public int getSum() {
-        return sum;
-    }
-
-    public double getAvg() {
-        return (double) sum / grades.size();
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s %.2f", id, getAvg());
-    }
-
-    @Override
-    public int compareTo(Student o) {
-        int avg = Double.compare(getAvg(), o.getAvg());
-        if (avg == 0)
-            return id.compareTo(o.id);
-        return -avg;
-    }
-}
